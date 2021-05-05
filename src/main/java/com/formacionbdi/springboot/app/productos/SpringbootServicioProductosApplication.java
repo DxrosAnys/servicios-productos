3ed5b2fa48c0dd6367ced2dd0c4430dbd017cf9b
DxrosAnys <diego.267.ri@gmail.com>
2021-05-05 00:18:03 -0500

    add dockerfile

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -1,6 +1,7 @@
 package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
